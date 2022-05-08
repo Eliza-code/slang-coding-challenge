fc763674e0dcd2cccb07ef117d4202a4da65d443
Eliza-code <lizcano.liz14@gmail.com>
2022-05-07 21:08:13 -0500

    add index

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const { AUTH_TOKEN } = require("./constants/index")
+const { getActivities, classifyActivitiesByUser } = require("./helpers/activitiesHelper")
+
 function main() {
-    console.log('Hello Slang');
+    return getActivities()
+    .then((response) => {
+        return classifyActivitiesByUser(response.activities)
+
+    } ) 
+    .then((sessions)=> postSessions(sessions))
+    .catch((error)=> console.log(error))
 }
 
 main();
