681adfbf2741bc5b6c5de8b6c66776fd6603c161
Eliza-code <lizcano.liz14@gmail.com>
2022-05-18 20:46:03 -0500

    refactor: fix indent in utils files

--- utils/fetchUtils.js
+++ utils/fetchUtils.js
@@ -1,24 +1,24 @@
 const { default: axios } = require("axios");
 
 async function fetchData(url, options) {
-    try {
-        const response = await axios.get(url, options)
-        return response.data; 
-    } catch(e) {
-        console.log(e);
-    }
+  try {
+    const response = await axios.get(url, options)
+    return response.data; 
+  } catch(e) {
+    console.log(e);
+  }
 }
 
 async function postData(url, body, options) {
-    try{
-        const response = await axios.post(url, body, options)
-        return response;
-    } catch(e) {
-        console.log(e);
-    }
+  try{
+    const response = await axios.post(url, body, options)
+    return response;
+  } catch(e) {
+    console.log(e);
+  }
 }
 
 module.exports = {
-    fetchData,
-    postData
+  fetchData,
+  postData
 }
--- utils/timeUtils.js
+++ utils/timeUtils.js
@@ -1,11 +1,9 @@
-const { convertToDecimal } = require("./mathUtils");
-
 function calculateDurationInSeconds(start, finish) {
   const startInSeconds = new Date(start).getTime() / 1000;
   const finishInSeconds = new Date(finish).getTime() / 1000;
-  return convertToDecimal(finishInSeconds - startInSeconds);
+  return finishInSeconds - startInSeconds;
 }
 
 module.exports = {
   calculateDurationInSeconds,
-};
+}
